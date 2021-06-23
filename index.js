3e308e550319094393ca79411a60b594e2aa04f4
mdquayle <misternopants@gmail.com>
2021-06-23 18:03:48 -0400

    commit tree 03b2d8be0b323feed0fc5af1a8a9b740a7453085

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, string){
+    const result = array.filter(word => word.toUpperCase() === string.toUpperCase())
+    return result;
+}
+
+function fuzzyMatch(array, string){
+    const result = array.filter(word => word.slice(0,string.length)===string.slice(0,string.length))
+    return result;
+}
+
+function matchName(array,string){
+    const result = array.filter(({name}) => name === string);
+    return result;
+}
